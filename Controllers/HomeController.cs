af6d8109139fe90129fd5ac9609c34129b777dee
Sergii-Right <sergii.ziborov@gmail.com>
2016-08-16 18:00:02 +0300

    test automatic deploy

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace dotnetcoretestheroku.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page. Test autodeploy";
 
             return View();
         }
